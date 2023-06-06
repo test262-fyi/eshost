fc126039f87948077bfbcadcc05e8c87fc174e4e
Simon Wanner <simon+git@skyrising.xyz>
2023-06-07 01:01:11 +0200

    libjs: Do not mock the $262 global
    With the command line option this is unnecessary.
    In fact the `var $262` declaration would hide the real $262.

--- runtimes/libjs.js
+++ runtimes/libjs.js
@@ -1,31 +1,3 @@
-if (!globalThis['$262']) {
-  function print(...args) {
-    console.log(...args);
-  }
-  var $262 = {
-    global: globalThis,
-    gc() {
-      return gc();
-    },
-    createRealm(options) {
-      throw new Test262Error('createRealm() not yet supported.');
-    },
-    evalScript(code) {
-      throw new Test262Error('evalScript() not yet supported.');
-    },
-    getGlobal(name) {
-      return this.global[name];
-    },
-    setGlobal(name, value) {
-      this.global[name] = value;
-    },
-    IsHTMLDDA() { return {}; },
-    get agent() {
-      throw new Test262Error('agent.* not yet supported.');
-    }
-  };
-}
-
 $262.destroy = () => {};
 
 $262.source = $SOURCE;
