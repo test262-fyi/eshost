193fbe0c1a77a6dacb93292d68fa018fa6d59bc0
Linus Groh <mail@linusgroh.de>
2023-09-26 20:30:10 +0100

    kiesel: tweak $262 object to use function property syntax (#4)

--- runtimes/kiesel.js
+++ runtimes/kiesel.js
@@ -2,29 +2,28 @@ function print(str) {
   Kiesel.print(str);
 }
 
-// TODO: Update this to `var` and function properties once supported
-globalThis.$262 = {
+var $262 = {
   global: globalThis,
-  gc: function gc() {
+  gc() {
     return Kiesel.gc.collect();
   },
-  createRealm: function createRealm(options) {
+  createRealm(options) {
     var realm = Kiesel.createRealm();
     realm.eval(this.source);
     realm.$262.source = this.source;
     return realm.$262;
   },
-  evalScript: function evalScript(code) {
+  evalScript(code) {
     return Kiesel.evalScript(code);
   },
-  getGlobal: function getGlobal(name) {
+  getGlobal(name) {
     return this.global[name];
   },
-  setGlobal: function setGlobal(name, value) {
+  setGlobal(name, value) {
     this.global[name] = value;
   },
-  destroy: function destroy() { /* noop */ },
-  IsHTMLDDA: function IsHTMLDDA() { return {}; },
+  destroy() { /* noop */ },
+  IsHTMLDDA() { return {}; },
   source: $SOURCE,
   agent: {},
 };
