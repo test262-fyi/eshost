8c0e9fa1753bd7d6a917bcce662a7dc8df5d4d6f
Aapo Alasuutari <aapo.alasuutari@gmail.com>
2025-02-25 22:40:53 +0200

    Update nova.js

--- runtimes/nova.js
+++ runtimes/nova.js
@@ -1,6 +1,19 @@
-globalThis.$262 = {
-    global: globalThis,
-    detachArrayBuffer: detachArrayBuffer,
+{
+  // Extra block to avoid any issues with concatenating source files together.
+  let buildHarness = (global) => {
+    const novaObj = global.__nova__;
+    delete global.__nova__;
 
-    destroy() {}
-};
+    global.$262 = global.Object();
+    global.$262.global = global;
+    global.$262.detachArrayBuffer = novaObj.detachArrayBuffer;
+    global.$262.createRealm = () => {
+      return buildHarness(novaObj.createRealm());
+    };
+    global.$262.evalScript = global.eval;
+    global.$262.destroy = () => {};
+    return global.$262;
+  };
+
+  buildHarness(globalThis);
+}
